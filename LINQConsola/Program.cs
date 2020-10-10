3fd44d6aa0d02bd9e84b1ea492482b609b17db2b
= <carlos_jhousef593@hotmail.com>
2020-10-09 23:58:48 -0500

    tipos genéricos

--- LINQConsola/Program.cs
+++ LINQConsola/Program.cs
@@ -11,19 +11,55 @@ namespace LINQConsola
     {
         static void Main(string[] args)
         {
-            using (var db = new CursosVirtualesEntities())
-            {
-                IEnumerable<Continentes> continentes =
-                    from cont in db.Continentes
-                    select cont;
+            //List<T>
 
-                foreach(var continente in continentes)
-                {
-                    Console.WriteLine($"ID: {continente.ContinenteId}, Nombre: {continente.Nombre}");
-                }
+            List<string> listaNombres = new List<string>()
+            {
+                "Carlos", "Jhosef", "Maria", "Ana", "Juanita"
+            };
+
+            //List<T>
+            List<Estudiante> listaEstudiantes = new List<Estudiante>()
+            {
+                new Estudiante{ EstudianteId = 1, Nombre = "Jose", Apellido = "Flores", Grado = 3},
+                new Estudiante{ EstudianteId = 2, Nombre = "Marcia", Apellido = "Villanueva", Grado = 6},
+                new Estudiante{ EstudianteId = 3, Nombre = "David", Apellido = "Fernandez", Grado = 1},
+                new Estudiante{ EstudianteId = 4, Nombre = "Rodrigo", Apellido = "Bueno", Grado = 3},
+                new Estudiante{ EstudianteId = 5, Nombre = "David", Apellido = "Fernandez", Grado = 1},
+                new Estudiante{ EstudianteId = 6, Nombre = "Walter", Apellido = "Olmos", Grado = 3},
+            };
+
+            //IEnumerable<T> 
+            IEnumerable<Estudiante> estudiantes = from estudiante in listaEstudiantes
+                                                  where estudiante.Grado == 3
+                                                  select estudiante;
+
+            foreach (Estudiante est in estudiantes)
+            {
+                Console.WriteLine($"ID: {est.EstudianteId}, Nombre: {est.Nombre} {est.Apellido}, Grado: {est.Grado}");
+            }
+
+            Console.WriteLine("\n");
+
+            //var
+            var estudantes2 = from estudiante in listaEstudiantes
+                              where estudiante.Grado != 3
+                              select estudiante;
+
+            foreach (Estudiante est in estudiantes)
+            {
+                Console.WriteLine($"ID: {est.EstudianteId}, Nombre: {est.Nombre} {est.Apellido}, Grado: {est.Grado}");
             }
 
             Console.ReadKey();
+        }
+
+        public class Estudiante
+        {
+            public int EstudianteId { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public int Grado { get; set; }
         }
     }
 }
