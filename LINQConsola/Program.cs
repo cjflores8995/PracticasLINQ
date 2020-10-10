fdcab33ccf502f28d69fcd59f9f8bcaf69536345
= <carlos_jhousef593@hotmail.com>
2020-10-09 22:48:38 -0500

    se agrega el proyecto de consola

---
+++ LINQConsola/Program.cs
@@ -0,0 +1,29 @@
+﻿using LINQDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new CursosVirtualesEntities())
+            {
+                IEnumerable<Continentes> continentes =
+                    from cont in db.Continentes
+                    select cont;
+
+                foreach(var continente in continentes)
+                {
+                    Console.WriteLine($"ID: {continente.ContinenteId}, Nombre: {continente.Nombre}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
