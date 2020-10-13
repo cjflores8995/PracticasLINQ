b121ffc255a67aec6ce3dd6c9dfcb600aa1d7fcd
= <carlos_jhousef593@hotmail.com>
2020-10-12 20:57:45 -0500

    operadores de ordenación

--- LINQConsola/Program.cs
+++ LINQConsola/Program.cs
@@ -1,4 +1,5 @@
-﻿using LINQDataAccess;
+﻿using LINQConsola.Operadores;
+using LINQDataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,35 +12,7 @@ namespace LINQConsola
     {
         static void Main(string[] args)
         {
-            int[] numeros = new int[] { 1, 4, 3, 56, 23, 22, 54, 87, 1, 33 };
-
-            //Sintaxis de consulta
-            IEnumerable<int> sintaxisConsulta =
-                from num in numeros
-                where num % 2 == 0
-                orderby num
-                select num;
-
-            Console.WriteLine("Sintaxis de consulta");
-
-            foreach (var numero in sintaxisConsulta)
-            {
-                Console.Write($"{numero} ");
-            }
-
-            //Sintaxis de metodo
-            IEnumerable<int> sintaxisMetodo = numeros
-                .Where(num => num % 2 == 0)
-                .OrderBy(num => num);
-
-            Console.WriteLine("\n\nSintaxis de método");
-
-            foreach (var numero in sintaxisConsulta)
-            {
-                Console.Write($"{numero} ");
-            }
-
-
+            new Ordenacion().Ordenar();
 
             Console.ReadKey();
         }
